ccdce209ac20e727b0b102cad9d5bee981a87a6c
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-02-13 23:29:31 +0200

    adding constants colors

---
+++ utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimary = Color(0xFFF52D54);
+const Color kSecondary = Color(0xFF2196F3);
+const kGrey = Color(0xFFF1F2F6);
+const Color kWhite = Color(0xFFFFFFFF);
+const kSecondaryText = Color(0xFF757575);
