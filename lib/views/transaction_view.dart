fb7596347e942a6f8f420352ca23a39de4acf714
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-02-14 04:29:54 +0200

    transaction view ui is completed

--- lib/views/transaction_view.dart
+++ lib/views/transaction_view.dart
@@ -1,10 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:katib_kashi/utils/constants.dart';
+import 'package:katib_kashi/views/home_view.dart';
+import 'package:katib_kashi/views/sign_up_view.dart';
 
-class TransactionView extends StatelessWidget {
+class TransactionView extends StatefulWidget {
   const TransactionView({super.key});
 
   @override
+  State<TransactionView> createState() => _TransactionViewState();
+}
+
+class _TransactionViewState extends State<TransactionView> {
+  List<String> buttons = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '',
+    '0',
+    'del'
+  ];
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: kPrimary,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 300,
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Container(
+                    width: 220,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: const LinearGradient(
+                        colors: [
+                          Colors.blue,
+                          Colors.purple,
+                        ],
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '5678',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: kWhite,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    'Money transfers from monzer card',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: kWhite,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    '\$50320',
+                    style: TextStyle(
+                      fontSize: 52.0,
+                      color: kWhite,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const Text(
+                    'Maximum Is 100,000',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: kDark,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: kWhite,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 400,
+                      child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          mainAxisExtent: 100,
+                        ),
+                        itemBuilder: (context, index) {
+                          final button = buttons[index];
+                          if (button == 'del') {
+                            return Icon(Icons.backspace);
+                          } else if (button == '') {
+                            return Text('');
+                          }
+                          return MyButton(
+                            label: button,
+                          );
+                        },
+                        itemCount: buttons.length,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 52,
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: kSecondary,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Text(
+                          'Next',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w600,
+                            color: kWhite,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key, required this.label});
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text(
+        label,
+        style: const TextStyle(
+          color: kDark,
+          fontSize: 32,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
   }
 }
