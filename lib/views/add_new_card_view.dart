f7b22b7b7f1f58ef60c3fa7db7835876b36794be
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-02-14 06:05:39 +0200

    add new card ui is set up

---
+++ lib/views/add_new_card_view.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:katib_kashi/utils/constants.dart';
+import 'package:katib_kashi/views/home_view.dart';
+
+class NewCardView extends StatelessWidget {
+  const NewCardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimary,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: kPrimary,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back,
+            color: kWhite,
+          ),
+        ),
+        title: Text(
+          'Add New Card',
+          style: TextStyle(color: kWhite),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height + 150,
+          child: Column(
+            children: [
+              const SizedBox(
+                width: double.infinity,
+                height: 200,
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: kWhite,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 25),
+                      TextField(
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(20),
+                          filled: true,
+                          fillColor: kGrey,
+                          hintText: 'Card Type',
+                          hintStyle: const TextStyle(
+                            color: kSecondaryText,
+                            fontSize: 16.0,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(20),
+                          filled: true,
+                          fillColor: kGrey,
+                          hintText: 'Full Name',
+                          hintStyle: const TextStyle(
+                            color: kSecondaryText,
+                            fontSize: 16.0,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(20),
+                          filled: true,
+                          fillColor: kGrey,
+                          hintText: 'Card Number',
+                          hintStyle: const TextStyle(
+                            color: kSecondaryText,
+                            fontSize: 16.0,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(20),
+                          filled: true,
+                          fillColor: kGrey,
+                          hintText: 'Current Balance',
+                          hintStyle: const TextStyle(
+                            color: kSecondaryText,
+                            fontSize: 16.0,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(20),
+                          filled: true,
+                          fillColor: kGrey,
+                          hintText: 'Phone Number',
+                          hintStyle: const TextStyle(
+                            color: kSecondaryText,
+                            fontSize: 16.0,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 35),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const HomeView(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          height: 62,
+                          width: double.infinity,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: kSecondary,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Text(
+                            'Add',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              color: kWhite,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
