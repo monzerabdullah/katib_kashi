ccac8abebacab71e1a9f51de0890c98612b72418
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-02-14 12:40:30 +0200

    update carousel slider img list with gradint containers

--- lib/views/widgets/custom_slider.dart
+++ lib/views/widgets/custom_slider.dart
@@ -1,27 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:katib_kashi/utils/constants.dart';
 
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+final List<String> cardList = [
+  'Card Number 1',
+  'Card Number 2',
+  'Card Number 3',
+  'Card Number 4',
+  'Card Number 5',
 ];
 
-final List<Widget> imageSliders = imgList
-    .map((item) => Container(
-          margin: const EdgeInsets.all(5.0),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-            child: Stack(
-              children: <Widget>[
-                Image.network(item, fit: BoxFit.cover, width: 1000.0),
-              ],
-            ),
+final List<Widget> cardsSlider = cardList
+    .map(
+      (card) => Container(
+        margin: const EdgeInsets.all(5.0),
+        width: 800,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: const LinearGradient(
+            colors: [
+              kPrimary,
+              kSecondary,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-        ))
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Text(
+                  card,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: kWhite,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                '****    ****    ****    8607',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: kWhite,
+                ),
+              ),
+              const SizedBox(height: 18),
+              const Text(
+                'Current Balance',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: kWhite,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              const Text(
+                '450,300900',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: kWhite,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    )
     .toList();
 
 class CustomSlider extends StatefulWidget {
@@ -41,7 +92,7 @@ class _CustomSliderState extends State<CustomSlider> {
       children: [
         const SizedBox(height: 50),
         CarouselSlider(
-          items: imageSliders,
+          items: cardsSlider,
           carouselController: _controller,
           options: CarouselOptions(
               autoPlay: false,
@@ -55,7 +106,7 @@ class _CustomSliderState extends State<CustomSlider> {
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: imgList.asMap().entries.map((entry) {
+          children: cardsSlider.asMap().entries.map((entry) {
             return GestureDetector(
               onTap: () => _controller.animateToPage(entry.key),
               child: Container(
