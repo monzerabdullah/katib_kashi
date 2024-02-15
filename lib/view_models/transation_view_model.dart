15750245568d32e1754a364f4dc23cf37bee021d
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-02-15 22:56:26 +0200

    transaction view model is created

--- lib/view_models/transation_view_model.dart
+++ lib/view_models/transation_view_model.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:katib_kashi/models/transaction.dart';
 
-class TransactionViewModel extends ChangeNotifier {}
+class TransactionViewModel extends ChangeNotifier {
+  List<Transaction> allTranasctions = [];
+
+  void addTransaction(Transaction transaction) {
+    allTranasctions.add(transaction);
+    notifyListeners();
+  }
+}
