326bed1950005b75177d4d5803ef8618b1d38315
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-02-16 15:07:53 +0200

    updating card model name because it confilcts with a widget name

--- lib/view_models/cards_view_model.dart
+++ lib/view_models/cards_view_model.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:katib_kashi/models/card.dart';
 
 class CardsViewModel with ChangeNotifier {
-  List<Card> allCards = [];
+  List<CardModel> allCards = [];
 
-  void addNewCard(Card card) {
+  void addNewCard(CardModel card) {
     allCards.add(card);
     notifyListeners();
   }
