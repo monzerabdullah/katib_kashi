89564212bd60beb6db49a8a60dfa98609451464a
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-02-16 10:40:46 +0200

    update the view model

--- lib/view_models/transation_view_model.dart
+++ lib/view_models/transation_view_model.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:katib_kashi/models/transaction.dart';
 
-class TransactionViewModel extends ChangeNotifier {
+class TransactionViewModel with ChangeNotifier {
   List<Transaction> allTranasctions = [];
 
   void addTransaction(Transaction transaction) {
