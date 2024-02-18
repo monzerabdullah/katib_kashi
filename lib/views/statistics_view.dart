43a1c4fce24ea8ad4de387e7d6e3afe06a13e7fc
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-02-18 11:28:36 +0200

    created statistics row widget and tabbar view

--- lib/views/statistics_view.dart
+++ lib/views/statistics_view.dart
@@ -8,113 +8,146 @@ class StatisticsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kWhite,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-            color: kPrimary,
-          ),
-        ),
-        title: const Text(
-          'Statistics',
-          style: TextStyle(
-            color: kPrimary,
-          ),
-        ),
-        centerTitle: true,
-        actions: const [
-          CircleAvatar(
-            radius: 25,
-            backgroundImage: AssetImage('assets/images/user_avatar.jpg'),
-          ),
-          SizedBox(
-            width: 15,
-          )
-        ],
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    child: Icon(
-                      Icons.insert_chart_rounded,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('Income'),
-                      SizedBox(height: 5),
-                      Text(
-                        '${Provider.of<TransactionViewModel>(context).income()}',
-                      ),
-                    ],
-                  ),
-                ],
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: kWhite,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back,
+                color: kPrimary,
               ),
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    child: Icon(
-                      Icons.insert_chart_rounded,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('Sent'),
-                      SizedBox(height: 5),
-                      Text(
-                        '${Provider.of<TransactionViewModel>(context).sent()}',
-                      ),
-                    ],
-                  ),
-                ],
+            ),
+            title: const Text(
+              'Statistics',
+              style: TextStyle(
+                color: kPrimary,
               ),
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    child: Icon(
-                      Icons.insert_chart_rounded,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('Recived'),
-                      SizedBox(height: 5),
-                      Text(
-                        '${Provider.of<TransactionViewModel>(context).recived()}',
-                      ),
-                    ],
-                  ),
-                ],
+            ),
+            centerTitle: true,
+            actions: const [
+              CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('assets/images/user_avatar.jpg'),
               ),
+              SizedBox(
+                width: 15,
+              )
             ],
+            bottom: const TabBar(
+              indicatorColor: kSecondary,
+              indicatorWeight: 3,
+              unselectedLabelColor: kDark,
+              labelColor: kSecondary,
+              labelStyle: TextStyle(
+                fontSize: 16,
+                color: kGrey,
+                fontWeight: FontWeight.w600,
+              ),
+              tabs: [
+                Tab(text: 'Day'),
+                Tab(text: 'Week'),
+                Tab(text: 'Month'),
+                Tab(text: 'Year'),
+              ],
+            ),
           ),
+          body: TabBarView(
+            children: [
+              StatisticsRow(),
+              StatisticsRow(),
+              StatisticsRow(),
+              StatisticsRow(),
+            ],
+          )),
+    );
+  }
+}
+
+class StatisticsRow extends StatelessWidget {
+  const StatisticsRow({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          children: [
+            CircleAvatar(
+              radius: 30,
+              child: Icon(
+                Icons.insert_chart_rounded,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Income'),
+                SizedBox(height: 5),
+                Text(
+                  '${Provider.of<TransactionViewModel>(context).income()}',
+                ),
+              ],
+            ),
+          ],
         ),
-      ),
+        Row(
+          children: [
+            CircleAvatar(
+              radius: 30,
+              child: Icon(
+                Icons.insert_chart_rounded,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Sent'),
+                SizedBox(height: 5),
+                Text(
+                  '${Provider.of<TransactionViewModel>(context).sent()}',
+                ),
+              ],
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            CircleAvatar(
+              radius: 30,
+              child: Icon(
+                Icons.insert_chart_rounded,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Recived'),
+                SizedBox(height: 5),
+                Text(
+                  '${Provider.of<TransactionViewModel>(context).recived()}',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
